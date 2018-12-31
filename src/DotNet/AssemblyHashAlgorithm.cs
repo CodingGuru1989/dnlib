b0f08d1ef8cea8b43c4893363f48b29dc4c993f0
Tianjiao <wangtianjiao.wang959@gmail.com>
2018-12-31 23:22:15 +0800

    Fix CA5351 and CA5350

--- src/DotNet/AssemblyHashAlgorithm.cs
+++ src/DotNet/AssemblyHashAlgorithm.cs
@@ -8,17 +8,7 @@
 		/// <summary/>
 		None				= 0,
 		/// <summary/>
-		MD2					= 0x8001,
-		/// <summary/>
-		MD4					= 0x8002,
-		/// <summary>This is a reserved value in the CLI</summary>
-		MD5					= 0x8003,
-		/// <summary>The only algorithm supported by the CLI</summary>
-		SHA1				= 0x8004,
-		/// <summary/>
 		MAC					= 0x8005,
-		/// <summary/>
-		SSL3_SHAMD5			= 0x8008,
 		/// <summary/>
 		HMAC				= 0x8009,
 		/// <summary/>
@@ -36,12 +26,7 @@
 	public static partial class Extensions {
 		internal static string GetName(this AssemblyHashAlgorithm hashAlg) {
 			switch (hashAlg) {
-			case AssemblyHashAlgorithm.MD2:		return null;
-			case AssemblyHashAlgorithm.MD4:		return null;
-			case AssemblyHashAlgorithm.MD5:		return "MD5";
-			case AssemblyHashAlgorithm.SHA1:	return "SHA1";
 			case AssemblyHashAlgorithm.MAC:		return null;
-			case AssemblyHashAlgorithm.SSL3_SHAMD5: return null;
 			case AssemblyHashAlgorithm.HMAC:	return null;
 			case AssemblyHashAlgorithm.TLS1PRF:	return null;
 			case AssemblyHashAlgorithm.HASH_REPLACE_OWF: return null;
